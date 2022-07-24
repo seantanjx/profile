089077574db41323bcf605f9ced740de5348ec3e
Sean Tan <seantan1710@gmail.com>
2022-07-24 15:37:41 +0800

    changed navbar color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -54,7 +54,7 @@ const NavBar = () => {
     <Navbar
       fixed="top"
       expand="md"
-      bg="dark"
+      bg="#121212"
       variant="dark"
       className="navbar-custom"
       expanded={expanded}
